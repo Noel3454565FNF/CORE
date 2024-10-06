e111b2d675aa536485dd2ed6e8aff470840c6f36
Noel345 <58745415+Noel3454565FNF@users.noreply.github.com>
2024-10-06 02:50:35 +0200

    not a lot

--- Assets/Scripts/MainElevatorManaher.cs
+++ Assets/Scripts/MainElevatorManaher.cs
@@ -8,6 +8,7 @@ public class MainElevatorManaher : NetworkBehaviour
 
     public GameObject[] MainElevatorContent;
     public GameObject[] MainElevatorBlastDoors;
+    private BlastDoorsInfo[] MainElevatorBlastDoorsSYS;
     public Vector3[] MainElevatorfloor;
 
     public string MainElevatorStatus = "IDLE";
@@ -20,7 +21,38 @@ public class MainElevatorManaher : NetworkBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        MainElevatorBlastDoorsSYS.Initialize();
+        int attempt = -1;
         GM = GameObject.Find("GameManager").GetComponent<GameManager>();
+        foreach(GameObject L in  MainElevatorBlastDoors)
+        {
+            if (attempt <= 0)
+            {
+                attempt += 1;
+            }
+            else if (attempt == -1)
+            {
+                attempt = 0;
+            }
+           MainElevatorBlastDoorsSYS[attempt] = L.GetComponent<BlastDoorsInfo>();
+        }
     }
 
+
+
+    public void GonnaMoveMainElev(int floor)
+    {
+        int diff = diffrence(floor, MainElevatorCurrentFloor);
+    }
+
+
+
+    public int diffrence(int num1, int num2)
+    {
+        int cout;
+
+        cout = Mathf.Max(num2, num1) - Mathf.Min(num1, num2);
+
+        return cout;
+    }
 }
